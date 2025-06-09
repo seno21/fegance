5ff479a5581d1bdf5b187340276c14a8e07be9a8
seno21 <fegasusena@gmail.com>
2025-06-09 20:06:25 +0700

    change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+    },
+  },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: [
